b0f794809a93706e2ea6f4a5bcb896421b9cdafc
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    shieldtablet: Import stock Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,12 @@
+#Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 # Recents
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.recents.grid=true
