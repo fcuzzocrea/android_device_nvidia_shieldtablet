4078b17744af399f2f21c296236a405e04188220
Aaron Kling <webgeek1234@gmail.com>
2019-01-03 22:27:35 -0600

    Add ardbeg support
    Change-Id: I620e52745735f3b1b45a0c71fc648b38b37a5ef9

--- device.mk
+++ device.mk
@@ -33,12 +33,16 @@ PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
 # Init related
 PRODUCT_PACKAGES += \
     init_tegra \
+    fstab.ardbeg \
     fstab.loki \
     fstab.tn8 \
+    init.recovery.ardbeg.rc \
     init.recovery.loki.rc \
     init.recovery.tn8.rc \
+    init.ardbeg.rc \
     init.loki.rc \
     init.tn8.rc \
     init.tn8_common.rc \
+    power.ardbeg.rc \
     power.loki.rc \
     power.tn8.rc
