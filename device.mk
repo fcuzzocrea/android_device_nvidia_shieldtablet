18d8ec1a2e8df7e90aa604c3cb83da9926fb10a1
Aaron Kling <webgeek1234@gmail.com>
2019-05-07 10:33:57 -0500

    Add ethernet capability
    Devices can use ethernet via a usb adapter
    
    Change-Id: Ibab34d303bcda106039788296e9a2202d30fbd40

--- device.mk
+++ device.mk
@@ -60,3 +60,7 @@ PRODUCT_PACKAGES += \
     power.loki.rc \
     power.tn8.rc \
     power.yellowstone.rc
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
